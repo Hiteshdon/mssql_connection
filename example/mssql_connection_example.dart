3d84bda26a67e957e3f28ecb4eec979b1c9df77b
Ankeshwarapu Hitesh <hiteshdon6@gmail.com>
2025-08-24 07:42:14 +0530

    Add FreeTDS build scripts for all platforms 1

---
+++ example/mssql_connection_example.dart
@@ -0,0 +1,6 @@
+import 'package:mssql_connection/mssql_connection.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/mssql_connection_test.dart
@@ -0,0 +1,16 @@
+import 'package:mssql_connection/mssql_connection.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
